02157cf448afdba649c1a9a8f9636129c527960f
Linus Marco <lmarco163@gmail.com>
2017-11-20 22:10:15 -0500

    lazy load components

--- app/src/main.js
+++ app/src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import component1 from './component1.vue';
-import component2 from './component2.vue';
-import component3 from './component3.vue';
 
-Vue.component('component1', component1);
-Vue.component('component2', component2);
-Vue.component('component3', component3);
+Vue.component('component1', () => import('./component1.vue'));
+Vue.component('component2', () => import('./component2.vue'));
+Vue.component('component3', () => import('./component3.vue'));
 
 new Vue({
     el: '#app',
